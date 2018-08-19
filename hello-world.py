ad996966869944f45b00de151e71dfbb75f497b2
Jack Merlin Bruce <33180920+jackmerlinbruce@users.noreply.github.com>
2018-08-19 17:47:15 +0100

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,3 @@
-print("And God said, hello-world.py")
-print("\n")
-print("You updated your GitHub repository.")
+print("And God said, hello-world.py\n")
+print("You updated your GitHub repository.\n")
+print("And now you have pulled from GitHub.\n")
